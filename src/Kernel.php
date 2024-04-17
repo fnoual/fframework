c04eabf098d58278f1e34046c418708ad335bc01
Fawzi Noual <fouzi.noual@gmail.com>
2024-04-17 23:40:34 +0100

    first commit

---
+++ src/Kernel.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Fframework;
+
+use Fframework\DependencyInjection\AppExtension;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+
+    public function registerBundles(): array
+    {
+        $bundles = [
+            new FrameworkBundle(),
+            new TwigBundle(),
+        ];
+
+        if ('dev' === $this->getEnvironment()) {
+            $bundles[] = new WebProfilerBundle();
+        }
+
+        return $bundles;
+    }
+
+    protected function build(ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->registerExtension(new AppExtension());
+    }
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->import(__DIR__.'/../config/app.yaml');
+
+        // register all classes in /src/ as service
+        $container->services()
+            ->load('Fframework\\', __DIR__.'/*')
+            ->autowire()
+            ->autoconfigure()
+        ;
+
+        // configure WebProfilerBundle only if the bundle is enabled
+        if (isset($this->bundles['WebProfilerBundle'])) {
+            $container->extension('web_profiler', [
+                'toolbar' => true,
+                'intercept_redirects' => false,
+            ]);
+        }
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        // import the WebProfilerRoutes, only if the bundle is enabled
+        if (isset($this->bundles['WebProfilerBundle'])) {
+            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')->prefix('/_wdt');
+            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
+        }
+
+        // load the routes defined as PHP attributes
+        // (use 'annotation' as the second argument if you define routes as annotations)
+        $routes->import(__DIR__.'/Controller/', 'attribute');
+    }
+
+    // optional, to use the standard Symfony cache directory
+    public function getCacheDir(): string
+    {
+        return __DIR__.'/../var/cache/'.$this->getEnvironment();
+    }
+
+    // optional, to use the standard Symfony logs directory
+    public function getLogDir(): string
+    {
+        return __DIR__.'/../var/log';
+    }
+}
